b2476cbf540a6e70d23ab113de9a7239d9db484a
wutong8023 <wutong8023@gmail.com>
2025-04-28 06:54:16 +0000

    Deploying to gh-pages from @ wutong8023/wutong8023.github.io@d6f908abd67466e6aae118bc6d5bd4dc401a4753 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,6 +72,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
+          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
