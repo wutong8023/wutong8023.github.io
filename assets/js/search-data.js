e3ba6a175aa404efe9e2a9fd8ac14e65ce9df944
wutong8023 <wutong8023@gmail.com>
2025-04-29 13:45:38 +0000

    Deploying to gh-pages from @ wutong8023/wutong8023.github.io@d59ca50c5d478e6557b33f16a97c0cd1e81374cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-team",
           title: "Team",
-          description: "",
+          description: "My brilliant collaborators and friends.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/Team/";
